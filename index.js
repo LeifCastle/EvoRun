8effe6596721e98e312040a3b93d51177e4d090b
LeifCastle <leifurcastle@gmail.com>
2023-04-01 21:03:21 -0700

    feat: added basic player to initial rendering

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const playButton = document.querySelector("#playButton");
 
 //Images
 const pathImage = document.querySelector("#path");
+const playerImage = document.querySelector("#player");
 
 //====== Global Variables ======\\
 
@@ -24,14 +25,29 @@ class Path {
     this.start = start;
     this.y = 0; //set to center variable later?
   }
-
   render() {
     ctx.drawImage(this.image, this.start, this.y);
   }
 }
 
+class Player {
+  constructor(image, x, y) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+  }
+  render() {
+    ctx.drawImage(this.image, this.x, this.y);
+  }
+}
+
 //====== Initialize Game ======\\
 function initializeGame() {
+  //Initialize entities to render
   const path = new Path(pathImage, 0);
+  const player = new Player(playerImage, 20, 60);
+
+  //Render entities
   path.render();
+  player.render();
 }
